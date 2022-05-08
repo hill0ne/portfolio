52a07c99b36f90235f326b1fdaa1221fcde01856
hill0ne <jeanj531@gmail.com>
2022-05-08 15:59:24 +0900

    [추가] about 페이지

--- src/component/header/header.jsx
+++ src/component/header/header.jsx
@@ -1,17 +1,26 @@
 import React from "react";
 import styles from "./header.module.css";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faHouse } from "@fortawesome/free-solid-svg-icons";
 
 function Header(props) {
   return (
     <>
       <div className={styles.header}>
-        <p className={styles.logo}>hill0ne</p>
+        <Link to="/about" className={styles.logo}>
+          About hill0ne
+        </Link>
         <FontAwesomeIcon
           icon={faBars}
           className={styles.bars}
         ></FontAwesomeIcon>
+        <Link to="/">
+          <FontAwesomeIcon
+            icon={faHouse}
+            className={styles.goHome}
+          ></FontAwesomeIcon>
+        </Link>
       </div>
     </>
   );
