73c1ddbdd9d65a724cf5374f110b40fc5a4e52fb
hill0ne <jeanj531@gmail.com>
2022-05-15 21:39:50 +0900

    [수정] 하단의 footer 영역
    - github 이동 버튼 추가

--- src/component/description-projects/carrotGame/videos.jsx
+++ src/component/description-projects/carrotGame/videos.jsx
@@ -13,8 +13,10 @@ const Videos = () => {
     <>
       <section className={styles.video__layout_vertical}>
         <span className={styles.desText}>
-          Javascript 로 구현한 당근 찾기 게임입니다. <br />
+          Javascript 로 구현한 당근 찾기 Web Game Application 입니다. <br />
           play 버튼과 stop 버튼으로 게임을 시작, 정지 할 수 있습니다. <br />
+          게임 플레이 중 정지 버튼을 클릭하면 게임이 일시정지 되며 <br />
+          Replay 의사를 묻는 팝업창이 뜨도록 하였습니다. <br />
         </span>
         <div className={styles.video__wrap}>
           <video
@@ -28,7 +30,7 @@ const Videos = () => {
       </section>
       <section className={styles.video__layout_vertical}>
         <span className={styles.desText}>
-          타이머 기능을 만들어 시간 내에 당근을 모두 없애면 게임에서 이기고
+          타이머 기능을 만들어 시간 내에 당근을 모두 없애면 게임에서 이기게 되고
           <br />
           win 문구의 팝업이 뜨도록 구현하였습니다.
         </span>
@@ -44,7 +46,7 @@ const Videos = () => {
       </section>
       <section className={styles.video__layout_vertical}>
         <span className={styles.desText}>
-          타이머 내에 당근을 없애지 못하거나, 벌레를 클리기하면 <br />
+          타이머 내에 당근을 없애지 못하거나 벌레를 클릭하면 <br />
           lose 문구의 팝업과 함께 게임이 종료됩니다.
         </span>
         <div className={styles.video__wrap}>
@@ -59,7 +61,8 @@ const Videos = () => {
       </section>
       <section className={styles.video__layout_vertical}>
         <span className={styles.desText}>
-          현재 모바일 반응형과 로그인 기능 등을 추가하는 리펙토링 중에 있습니다.
+          현재 모바일 웹 버전과 로그인 기능 등을 추가하는 리펙토링 중에
+          있습니다.
         </span>
       </section>
       <footer className={styles.footer}></footer>
